dd272de8580f21eb730aeac40cb01986ca4637b3
Marco Greco <marcogrecopriolo@gmail.com>
2022-12-10 12:28:39 +0000

    macos port and qt6

--- include/backend/mm_malloc.h
+++ include/backend/mm_malloc.h
@@ -39,7 +39,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #if defined(_WIN32) || defined(_WIN64)
 #define MALLOC(a) _mm_malloc(a, 16)
-#elifndef __OSX__
+#elif !defined(__APPLE_CC__)
 #include <malloc.h>
 #define MALLOC(a) memalign(16, a)
 #endif
