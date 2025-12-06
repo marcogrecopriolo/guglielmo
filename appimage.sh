417e905a6bc1dacb490d9f92a2a3d56246a2320a
Marco Greco <marcogrecopriolo@gmail.com>
2025-12-06 20:58:30 +0000

    RPI appimage

--- appimage.sh
+++ appimage.sh
@@ -1,13 +1,15 @@
 version=`awk '/^objectVersion.*=/ { print $3 }' guglielmo.pro`
+arch=`uname -m`
 mkdir  build
 cd build
 cmake .. -DRTLSDR=ON -DAIRSPY=ON -DSDRPLAY=ON -DSDRPLAY_V3=ON -DLIMESDR=ON -DHACKRF=ON -DCMAKE_INSTALL_PREFIX=/usr
 make -j4
 cd ..
-mkdir -p appdir/usr/bin
 cp build/guglielmo  appdir/usr/bin/guglielmo
 mkdir -p appdir/usr/lib
-cp /usr/lib64/librtlsdr.so  appdir/usr/lib
+
+# look for local first, and quit after the first one
+cp `find -L /usr/local /usr -name librtlsdr.so -print -quit 2>&-`  appdir/usr/lib
  
 mkdir -p appdir/usr/share/applications
 cp etc/guglielmo.desktop appdir/usr/share/applications
@@ -16,16 +18,16 @@ touch appdir/guglielmo.png
 mkdir -p appdir/usr/share/icons/hicolor/256x256/apps/
 cp images/guglielmo.png appdir/usr/share/icons/hicolor/256x256/apps/
   
-wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage" 
+wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-${arch}.AppImage" 
 chmod a+x linuxdeployqt*.AppImage
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
-./linuxdeployqt*.AppImage ./appdir/usr/share/applications/* -bundle-non-qt-libs -no-translations -extra-plugins=styles
-./linuxdeployqt*.AppImage --appimage-extract
+./linuxdeployqt-continuous-${arch}.AppImage ./appdir/usr/share/applications/* -unsupported-allow-new-glibc -bundle-non-qt-libs -no-translations -extra-plugins=styles
+./linuxdeployqt-continuous-${arch}.AppImage --appimage-extract
 find appdir/usr/plugins/ -type f -exec squashfs-root/usr/bin/patchelf --set-rpath '$ORIGIN/../../lib' {} \;
 chmod a+x etc/appimage/* ; rm appdir/AppRun ; cp etc/appimage/* appdir/
 export PATH=squashfs-root/usr/bin/:$PATH
 squashfs-root/usr/bin/appimagetool $(readlink -f ./appdir/)
 if test "$version" != ""
 then
-    mv guglielmo-x86_64.AppImage guglielmo-x86_64-v$version.AppImage
+    mv guglielmo-${arch}.AppImage guglielmo-${arch}-v${version}.AppImage
 fi
