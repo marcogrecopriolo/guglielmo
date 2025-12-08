0f0350f3bae0f48ccf4cb9c89f28dbc79d2b270c
Marco Greco <marcogrecopriolo@gmail.com>
2025-12-08 18:07:19 +0000

    yet more appimage changes

--- appimage.sh
+++ appimage.sh
@@ -5,23 +5,22 @@ cd build
 cmake .. -DRTLSDR=ON -DAIRSPY=ON -DSDRPLAY=ON -DSDRPLAY_V3=ON -DLIMESDR=ON -DHACKRF=ON -DCMAKE_INSTALL_PREFIX=/usr
 make -j4
 cd ..
+mkdir -p appdir/usr/bin appdir/usr/lib appdir/usr/plugin appdir/usr/share/applications appdir/usr/share/icons/hicolor/256x256/apps
 cp build/guglielmo  appdir/usr/bin/guglielmo
-mkdir -p appdir/usr/lib
 
 # look for local first, and quit after the first one
 cp `find -L /usr/local /usr -name librtlsdr.so -print -quit 2>&-`  appdir/usr/lib
+cp `find -L /usr/local /usr -name libmpris-qt5.so -print -quit 2>&-`  appdir/usr/lib
  
-mkdir -p appdir/usr/share/applications
 cp etc/guglielmo.desktop appdir/usr/share/applications
 cp images/guglielmo.png appdir/guglielmo.png
 touch appdir/guglielmo.png
-mkdir -p appdir/usr/share/icons/hicolor/256x256/apps/
 cp images/guglielmo.png appdir/usr/share/icons/hicolor/256x256/apps/
   
 wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-${arch}.AppImage" 
 chmod a+x linuxdeployqt*.AppImage
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
-./linuxdeployqt-continuous-${arch}.AppImage ./appdir/usr/share/applications/* -unsupported-allow-new-glibc -bundle-non-qt-libs -no-translations -extra-plugins=styles
+./linuxdeployqt-continuous-${arch}.AppImage ./appdir/usr/share/applications/* -verbose=2 -bundle-non-qt-libs -no-translations -extra-plugins=styles
 ./linuxdeployqt-continuous-${arch}.AppImage --appimage-extract
 find appdir/usr/plugins/ -type f -exec squashfs-root/usr/bin/patchelf --set-rpath '$ORIGIN/../../lib' {} \;
 chmod a+x etc/appimage/* ; rm appdir/AppRun ; cp etc/appimage/* appdir/
